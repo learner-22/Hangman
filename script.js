da1129682e7476b23dd8ac3ec6738d66c4c4deaa
learner-22 <nbhooma@gmail.com>
2022-05-12 10:14:45 -0400

    comments and sound effects

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let life = 5  //Total lives in the game
 let lifecountdown=document.getElementById('countdown') // The label element that racks the life countdown
 let footerImg = document.getElementById('footer2')
 
+let audio= new Audio("./Images/drip.mp3")
 //Displaying the Initial screen on the load of windows
 window.onload = function(){
     alphabetsArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
@@ -23,29 +24,30 @@ window.onload = function(){
     //Randomly choose a question and corresponding answer
     selectQuestion()
     displayAnswer()
-
+    //Variables to track, user guess and match with answer
     let answerLength =  answer[gameNumber].length
     let answerArray =[]
     let remainingLetters = answerLength
-    //Verifying userChoice with answer
+    //Verifying userChoice with answer, tracks if the user anser is correct and the answer is updated
     let updated = 0
-  
     var img=document.createElement("img");
     
+    //Event listener to capture the li selected by user and verifying if the guess is correct. If the guess is correct, the answer is displayed else the hangman graphic is shown
+
     alphabetsUl.addEventListener('click', function(e){
+   audio.play()
     let answerli = document.getElementById('answer').querySelectorAll('li')
-   
-      console.log(e)
+    console.log(e)
     console.log(answerli)    
     console.log(lifecountdown)
 
-    let guess = e.target.textContent
+    let guess = e.target.textContent //The letter guessed by the user
     if (life > 0 && remainingLetters >0)
     {
         updated = 0
-        console.log(`Updated in outer if ${updated}`) 
         console.log(`life ${life}`)
         console.log(guess)
+        //loop the word letters and if guess matches, populate the answer array with matches at all letter position in the word 
         for(i=0 ; i< answerLength ;i++){
           
             if (answer[gameNumber].charAt(i) === guess){
@@ -63,23 +65,26 @@ window.onload = function(){
                    continue
             }         
         }
+        //If guess is incorrect the life is decremented and image updated
         if( updated === 0){
             life--
             lifecountdown.textContent = 'You have '+ life + ' lives remaining'
             img.src=`./Images/img${5-life}.jpg`
             footerImg.appendChild(img)
         }    
-    // }
-    // else{
-        if(life ==0){
+   //Action to be taken when the game is lost
+        if(life == 0){
         console.log('All lives taken')
        // alert ('All lives taken, Hit Reset to continue')
-       
+       statusMessage.textContent = 'All lives taken, Hit Reset to continue!!' 
+       modal.style.display = "block";
         }
+    //Action to be taken when the game is won
         if(remainingLetters <= 0){
         console.log('Word Completed')
         //alert ('You Win the Game!!! Next Question is here for you')
-        
+        statusMessage.textContent = 'You Win the Game!!!Hit Reset for the Next Question' 
+        modal.style.display = "block";
         }
     }
 
@@ -89,73 +94,75 @@ window.onload = function(){
      // Capturing the reset event click
      let resetButton = document.getElementById('reset')
      resetButton.addEventListener('click', resetScreen)
- // Code for reset button
- function resetScreen(){
-    questionDiv.removeChild(questionDiv.firstElementChild)
-    selectQuestion()
-    answerDiv.innerHTML =''
-    displayAnswer()
-    hintp.innerHTML =''
-    lifecountdown.textContent =' Life CountDown'
-    answerLength =  answer[gameNumber].length
-    answerArray =[]
-    remainingLetters = answerLength
-    updated = 0
-    life = 5
-    alphabetsUl.innerHTML =''
-    footerImg.innerHTML =''
-    for(let i=0; i<alphabetsArray.length; i++){
+    // Code for reset button
+     // Getting a random question and corresponding answer. Updating the life Countdown, Other variables
+     //Recreating the alphabets UI with the letters , Removing the image
+    function resetScreen(){
+        questionDiv.removeChild(questionDiv.firstElementChild)
+        selectQuestion()
+        answerDiv.innerHTML =''
+        displayAnswer()
+        hintp.innerHTML =''
+        lifecountdown.textContent =' Life CountDown'
+        answerLength =  answer[gameNumber].length
+         answerArray =[]
+         remainingLetters = answerLength
+         updated = 0
+        life = 5
+        alphabetsUl.innerHTML =''
+        footerImg.innerHTML =''
+         for(let i=0; i<alphabetsArray.length; i++){
  
-        let list = document.createElement('li'); //the li's in alphabet UL
-        list.id = 'letter'; // settthe id to the li's
-        list.innerHTML = alphabetsArray[i].toUpperCase();
-        alphabetsUl.appendChild(list);
+             let list = document.createElement('li'); //the li's in alphabet UL
+             list.id = 'letter'; // settthe id to the li's
+             list.innerHTML = alphabetsArray[i].toUpperCase();
+             alphabetsUl.appendChild(list);
 
+         }
     }
- }
 
 }
 
+//Hangman Questions,Answers and Hints ....Basic Setup
 let question =[' Name of a state in US','An NBA Team name','Name of an endangered animal','Which state in US is called the Sunshine State','Which state in US is called The Peach State','Which state in US is called The Grand Canyon State','An extinct Bird']
 let answer =[ 'ATLANTA','LALAKERS','KIWI','FLORIDA','GEORGIA','ARIZONA','DODO']
 let hintresponse =['A Southern City in USA','Remember James!','Similar to a fruit' ,'Down South','Home of hawks','Answer is in the quesion','DO is the key']
 let gameNumber 
 
+//Code to select a random Question
 let questionDiv = document.getElementById('question')
 function selectQuestion(){
-    //let question =[' Name of a state in US','An NBA Team name','Name of an extinct animal']
-    
+      
     let p = document.createElement('p')
     questionDiv.appendChild(p)
-    gameNumber = Math.floor(Math.random()*5)
+    gameNumber = Math.floor(Math.random()*question.length)
     p.textContent = question[gameNumber]
 }
 
+
+//Code to display the answer word as blanks
 let answerDiv = document.getElementById('answer')
 function displayAnswer(){
     let answerLength= answer[gameNumber].length
-    //console.log(answerLength)
     let answerDiv = document.getElementById('answer')
 
-for(let i=0; i<answerLength; i++){
+    for(let i=0; i<answerLength; i++){
  
-    let answerlist = document.createElement('li');
-    answerlist.id = 'ansletter';
-    answerlist.innerHTML = '_';
-    answerDiv.appendChild(answerlist);
+        let answerlist = document.createElement('li');
+        answerlist.id = 'ansletter';
+        answerlist.innerHTML = '_';
+        answerDiv.appendChild(answerlist);
 
+    }
 }
 
-
-}
+//Code to populate the corresponding hint
 let hintresponseDiv= document.getElementById('hintresponse')
 let hintp = document.createElement('p')
 hintresponseDiv.appendChild(hintp)
 function hintClick(){
 
-//console.log(hintresponse[gameNumber])
-//let hintp = document.createElement('p')
-hintp.innerHTML = 'Your Hint -- '+ hintresponse[gameNumber]
+    hintp.innerHTML = 'Your Hint -- '+ hintresponse[gameNumber]
 
 }
 
@@ -163,4 +170,15 @@ let hintButton = document.getElementById('hint')
 hintButton.addEventListener('click', hintClick)
 
 
-
+//Code for Modal to display win and loose Status
+let modal =document.getElementById("modal1")
+let span =document.getElementsByClassName('close')[0];
+let statusMessage =document.getElementById('gameStatus')
+span.onclick = function(){
+    modal.style.display = "none"
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
